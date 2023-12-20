e168a63e721605fcbba9cadd05a7dacea51f6d75
sulton <global-sultonmalik326@gmail.com>
2023-12-20 16:21:56 +0500

    Add project files.

---
+++ Dars/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Dars
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+       
+          
+            Account account1 = new Account();
+
+            account1.Name = "user";
+            account1.Age = 30;
+            account1.Login = "Test";
+            account1.Password = "Asdfg1@5";
+
+            account1.PrintAccountInformation("Asdfg1@5");
+
+            
+
+            
+
+           
+
+
+        }
+    }
+}
